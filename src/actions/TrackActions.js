6ee2af486a31c010450272b93d5966094fe08f6e
David Kent <davidkent@fastmail.com.au>
2017-08-26 21:11:09 +1000

    Try and handle token expiry gracefully.

--- src/actions/TrackActions.js
+++ src/actions/TrackActions.js
@@ -8,19 +8,20 @@ import Account from '../data/Account';
 function refreshLogin(dispatch) {
   const auth = store.getState().auth;
   if (!auth.is_logged_in) {
-    return;
+    return false;
   }
   let now = new Date();
   // 1. check if we are expired - clear auth
   if (now > auth.expires) {
       console.log("Auth expired. Expiry: " + auth);
       TrackActions.logout();
+      return false;
   }
 
   // 2. check if more than 5 mins until expire - don't refresh
   if ((auth.expires.getTime() - now.getTime()) > 300000 ) {
       // Hasn't expired and won't in next five minutes so bail.
-      return;
+      return true;
   }
 
   // 3. not expired, but near expiry - refresh
@@ -40,6 +41,7 @@ function refreshLogin(dispatch) {
       username,
     });
   });
+  return true;
 }
 
 const TrackActions = {
@@ -55,7 +57,9 @@ const TrackActions = {
     const auth_token = store.getState().auth.token;
     const page_size = store.getState().transactions.page_size;
     return (dispatch) => {
-      refreshLogin(dispatch);
+      if (!refreshLogin(dispatch)) { 
+        return;
+      };
       return CatTrackAPI
         .get('/api/transactions/', {page: page_num, page_size: page_size}, auth_token)
         .then(rawTransactions => {
