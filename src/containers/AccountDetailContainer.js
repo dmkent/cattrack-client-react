9dc81bb229ae28c9d3cb0932b4df8277bb0e94f0
David Kent <davidkent@fastmail.com.au>
2017-09-17 09:50:50 +1000

    Test the Account Detail Container.

--- src/containers/AccountDetailContainer.js
+++ src/containers/AccountDetailContainer.js
@@ -1,14 +1,14 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import TrackActions from '../actions/TrackActions'
 import AccountDetail from '../views/AccountDetail'
 
-const mapStateToProps = state => {
+export function mapStateToProps(state) {
   return {
     accounts: state.accounts,
   }
 }
 
-const mapDispatchToProps = dispatch => {
+export function mapDispatchToProps(dispatch) {
   return {
     uploadToAccount: (account, upload_file) => {
       dispatch(TrackActions.uploadToAccount(account, upload_file))
---
+++ src/containers/__tests__/TestAccountDetailContainer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Immutable from 'immutable'
+import TrackActions from '../../actions/TrackActions'
+import {mapStateToProps, mapDispatchToProps} from '../../containers/AccountDetailContainer'
+
+jest.mock('../../actions/TrackActions', () => ({default: {
+  uploadToAccount: jest.fn(),
+}, __esModule: true}))
+
+describe('AccountDetailContainer', () => {
+  it('only have accounts in props', () => {
+    const initState = {
+      transactions: new Immutable.OrderedMap(),
+      categories: {
+        is_valid: {
+          valid: null,
+          message: ""
+        },
+        show_categorisor: false,
+        categories: new Immutable.List(),
+      },
+      accounts: {
+        accounts: [1],
+        upload_in_progress: false
+      }
+    }
+    expect(mapStateToProps(initState)).toEqual({
+      accounts: {
+        accounts: [1],
+        upload_in_progress: false,
+      }
+    })
+  })
+
+  it('maps dispatch to props', () => {
+    const dispatch = jest.fn()
+    const props = mapDispatchToProps(dispatch)
+    props.uploadToAccount()
+    expect(dispatch.mock.calls.length).toBe(1)
+    expect(TrackActions.uploadToAccount.mock.calls.length).toBe(1)
+  })
+})
