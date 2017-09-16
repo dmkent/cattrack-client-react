3e0c7fa1c774b079be9f9cfef4d2b22f11e0653c
David Kent <davidkent@fastmail.com.au>
2017-09-17 09:53:27 +1000

    Tests for AccountsContainer.

---
+++ src/containers/__tests__/TestAccountsContainer.js
@@ -0,0 +1,47 @@
+import Immutable from 'immutable'
+import TrackActions from '../../actions/TrackActions'
+import {mapStateToProps, mapDispatchToProps} from '../../containers/AccountsContainer'
+
+jest.mock('../../actions/TrackActions', () => ({default: {
+  loadAccounts: jest.fn(),
+}, __esModule: true}))
+
+describe('AccountsContainer', () => {
+  it('only have accounts and auth in props', () => {
+    const initState = {
+      auth: {
+        is_logged_in: true
+      },
+      transactions: new Immutable.OrderedMap(),
+      categories: {
+        is_valid: {
+          valid: null,
+          message: ""
+        },
+        show_categorisor: false,
+        categories: new Immutable.List(),
+      },
+      accounts: {
+        accounts: [1],
+        upload_in_progress: false
+      }
+    }
+    expect(mapStateToProps(initState)).toEqual({
+      auth: {
+        is_logged_in: true,
+      },
+      accounts: {
+        accounts: [1],
+        upload_in_progress: false,
+      }
+    })
+  })
+
+  it('maps dispatch to props', () => {
+    const dispatch = jest.fn()
+    const props = mapDispatchToProps(dispatch)
+    props.loadAccounts()
+    expect(dispatch.mock.calls.length).toBe(1)
+    expect(TrackActions.loadAccounts.mock.calls.length).toBe(1)
+  })
+})
