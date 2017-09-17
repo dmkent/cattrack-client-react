e2f25aa03837dfd1275f80e1964270dc6418dbfa
David Kent <davidkent@fastmail.com.au>
2017-09-17 10:22:37 +1000

    Tests for AppContainer.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,15 +1,15 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import TrackActions from '../actions/TrackActions'
 import AppView from '../views/AppView'
 
-const mapStateToProps = state => {
+export function mapStateToProps(state) {
   return {
     auth: state.auth,
     ...state.app
   }
 }
 
-const mapDispatchToProps = dispatch => {
+export function mapDispatchToProps(dispatch) {
   return {
     onLogin: (username, password) => {
       dispatch(TrackActions.attemptLogin(username, password))
